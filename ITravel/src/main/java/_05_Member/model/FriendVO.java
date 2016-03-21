36ceae5a9f2092c2d785cfbf3fb884ac216684a7
upah <zoe1232010@gmail.com>
2016-03-21 16:37:30 +0800

    新增MemberVO 新增CollectionVO 新增FriendVO 新增TrackVO

--- ITravel/src/main/java/_05_Member/model/FriendVO.java
+++ ITravel/src/main/java/_05_Member/model/FriendVO.java
@@ -3,5 +3,32 @@ package _05_Member.model;
 import java.io.Serializable;
 
 public class FriendVO implements Serializable {
-
+	private Integer friendNo;//編號
+	private Integer memberId;//會員編號
+	private Integer firendId;//好友會員編號
+	@Override
+	public String toString() {
+		return "FriendVO [friendNo=" + friendNo + ", memberId=" + memberId
+				+ ", firendId=" + firendId + "]";
+	}
+	public Integer getFriendNo() {
+		return friendNo;
+	}
+	public void setFriendNo(Integer friendNo) {
+		this.friendNo = friendNo;
+	}
+	public Integer getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+	public Integer getFirendId() {
+		return firendId;
+	}
+	public void setFirendId(Integer firendId) {
+		this.firendId = firendId;
+	}
+	
+	
 }
