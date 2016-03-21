cae507f6af033da6e50fd5913a0b47dae24aaee8
upah <zoe1232010@gmail.com>
2016-03-21 21:43:35 +0800

    在model 新增Member_interface 在model.dao中 新增CollectionDAOHibernate 新增MemberDAOHibernate

---
+++ ITravel/src/main/java/_05_Member/model/dao/CollectionDAOHibernate.java
@@ -0,0 +1,10 @@
+package _05_Member.model.dao;
+
+public class CollectionDAOHibernate {
+
+	public static void main(String[] args) {
+		CollectionDAOHibernate dao  =new CollectionDAOHibernate();
+
+	}
+
+}
---
+++ ITravel/src/main/java/_05_Member/model/dao/MemberDAOHibernate.java
@@ -0,0 +1,103 @@
+package _05_Member.model.dao;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import java.util.*;
+
+import _00_Misc.HibernateUtil_H4_Ver1;
+import _05_Member.model.MemberVO;
+import _05_Member.model.Member_interface;
+
+public class MemberDAOHibernate implements Member_interface  {
+
+	private static final String GET_ALL_STMT = "from MemberVO order by memberId";
+		
+	MemberVO memberVO = null;
+	
+	@Override
+	public MemberVO findByPrimaryKey(Integer memberId){
+		
+		Session session = HibernateUtil_H4_Ver1.getSessionFactory().getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			memberVO = (MemberVO) session.get(MemberVO.class, memberId);
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			session.getTransaction().rollback();
+			throw e;
+		}
+		
+		return memberVO;
+	}
+	
+	@Override
+	public  void insert(MemberVO vo){
+		Session session = HibernateUtil_H4_Ver1.getSessionFactory().getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			session.saveOrUpdate(memberVO);
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			session.getTransaction();
+			throw e;
+		}
+	}
+	@Override
+	public  void update (MemberVO vo) {
+		Session session = HibernateUtil_H4_Ver1.getSessionFactory().getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			session.saveOrUpdate(memberVO);
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			session.getTransaction().rollback();
+			throw e;
+			
+		}
+	}
+	@Override
+	public void delete (Integer memberId){
+		Session session = HibernateUtil_H4_Ver1.getSessionFactory().getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			MemberVO memberVO = new MemberVO();
+			memberVO.setMemberId(memberId);
+			session.delete(memberVO);
+			session.getTransaction();
+		} catch (RuntimeException e) {
+			session.getTransaction().rollback();
+			throw e;	
+		}
+	}
+	
+	@Override
+	public List<MemberVO> getall(){
+		List<MemberVO> list = null;
+		Session session = HibernateUtil_H4_Ver1.getSessionFactory().getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery(GET_ALL_STMT);
+			list = query.list();
+			session.getTransaction();
+		} catch (RuntimeException e) {
+			session.getTransaction().rollback();
+			throw e;
+		}
+		return list;
+	}
+	public static void main (String [] args) {
+		
+		MemberDAOHibernate dao = new MemberDAOHibernate();
+		
+		
+		
+		
+	}
+	
+}
