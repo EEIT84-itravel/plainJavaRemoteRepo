182398827c2c2cec98a09a6b8444cb3a8469fac9
fox <fox@III-EEIT>
2016-03-21 13:14:35 +0800

    modify codeVO,LabelTripAndJournalVO,ForumVO

--- ITravel/src/main/java/_00_Misc/CodeVO.java
+++ ITravel/src/main/java/_00_Misc/CodeVO.java
@@ -4,8 +4,9 @@ import java.io.Serializable;
 
 public class CodeVO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+	//代碼編號
 	private String codeId;
+	//代碼內容
 	private String codeName;
 
 	public String getCodeId() {
