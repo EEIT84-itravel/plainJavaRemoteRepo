91f92d1f08510d5343dcf186a79d3091197624e7
upah <zoe1232010@gmail.com>
2016-03-25 18:38:01 +0800

    新增

--- ITravel/src/main/java/_05_Member/model/Collection_interface.java
+++ ITravel/src/main/java/_05_Member/model/Collection_interface.java
@@ -6,7 +6,7 @@ public interface Collection_interface {
 	public void insert(CollectionVO vo);
 	public void update(CollectionVO vo);
 	public void delete (Integer CollectionNo);
-	public MemberVO findByPrimaryKey(Integer CollectionNo);
+	public CollectionVO findByPrimaryKey(Integer CollectionNo);
 	public List<CollectionVO> getall();
 	
 	
